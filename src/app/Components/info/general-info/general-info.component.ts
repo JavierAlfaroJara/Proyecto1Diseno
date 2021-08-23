8f58a02397feaa106dddf432fd38655b0be314a0
Javier Alberto Alfaro Jara <javier.alfaro.jara@gmail.com>
2021-08-23 02:16:44 -0600

    Vista info general concluida

--- src/app/Components/info/general-info/general-info.component.ts
+++ src/app/Components/info/general-info/general-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AudioDBService } from 'src/app/services/audioDb-service/audio-db.service';
 
 @Component({
   selector: 'app-general-info',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralInfoComponent implements OnInit {
 
-  constructor() { }
+  imagenLink = ""
+  nombre = ""
+  estilo = ""
+  genero = ""
+  annoFormacion = ""
+  about = ""
+  esBanda = false
+
+  search = ""
+
+  constructor(
+    private audioDB: AudioDBService
+  ) {this.search = "metallica" }
 
   ngOnInit(): void {
+    this.audioDB.getArtistDetails(this.search).subscribe((response)=>{
+      console.log(response);
+      this.imagenLink = response.artists[0].strArtistLogo;
+      this.nombre = response.artists[0].strArtist;
+      this.estilo = response.artists[0].strGenre;
+      if(response.artists[0].intMembers != "1"){
+        this.esBanda = true
+      }
+      if(response.artists[0].strGender == "Male"){
+        this.genero = "Hombre"
+      }
+      if(response.artists[0].strGender == "Female"){
+        this.genero = "Mujer"
+      }
+      this.annoFormacion = response.artists[0].intFormedYear;
+      this.about = response.artists[0].strBiographyEN;
+    });
   }
 
 }
