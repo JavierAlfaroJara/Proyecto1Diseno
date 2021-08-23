e678217fe7374704a3a8a9881dc095049d255454
Fidel Condega Alvarado <Tcondega25@estudiantec.cr>
2021-08-22 18:02:06 -0600

    Cambios minimos

--- src/app/Components/main/main.component.ts
+++ src/app/Components/main/main.component.ts
@@ -3,6 +3,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+/** Alertas */
+import Swal from 'sweetalert2';
+
+
 /** Services */
 import { AudioDBService } from 'src/app/services/audioDb-service/audio-db.service';
 
@@ -15,15 +19,27 @@ import { AudioDBService } from 'src/app/services/audioDb-service/audio-db.servic
 export class MainComponent implements OnInit {
   search = ""; 
 
-  constructor(private audioDB: AudioDBService) { }
+  constructor(
+    private audioDB: AudioDBService
+    ) { }
 
   ngOnInit(): void {
   }
 
   /** Metodo para buscar la información del artista */
   searchArtist(){
-    this.audioDB.getArtistDetails(this.search).subscribe((response)=>{
-      console.log(response)
-    });
+    try{
+      this.audioDB.getArtistDetails(this.search).subscribe((response)=>{
+        console.log(response)
+      });
+    }catch(error){
+      Swal.fire({
+        title: 'Error',
+        text: 'Nombre mal escrito o no hay información ',
+        icon: 'error',
+        confirmButtonText: 'coll'
+      })
+    }
+    
   }
 }
